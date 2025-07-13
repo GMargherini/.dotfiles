db3b7c66d36106678f9427466676d05dadbb4c64
Gmargherini <giorgio.margherini@gmail.com>
2025-07-13 14:41:29 +0200

    initial commit

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -g fish_greeting (fuzzy_clock)
+# starship preset nerd-font-symbols -o ~/.config/starship.toml
+# starship preset catppuccin-powerline -o ~/.config/starship.toml
+
+# starship init fish | source
